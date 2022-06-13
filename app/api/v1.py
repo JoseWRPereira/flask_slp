8d9fccc31eeb2aacbd24d6ad55f7f127a5e1f593
JoseWRPereira <josewrpereira@tutanota.com>
2022-06-13 18:44:44 -0300

    Inclusão de página de ajuda (help) para documentar a API

--- app/api/v1.py
+++ app/api/v1.py
@@ -1,14 +1,14 @@
 from flask import Blueprint
 from flask import make_response, jsonify, json
+from flask import render_template
 from app.db.dbcon import DBConn
 
-api_bp = Blueprint('api_bp', __name__)
+api_bp = Blueprint('api_bp', __name__,template_folder='templates')
 
+@api_bp.route('/api/ajuda')
 @api_bp.route('/api/help')
 def api_help():
-    response = make_response( jsonify({"Nome": "José"}), 200 )
-    response.headers["Content-Type"] = "application/json"
-    return response
+    return render_template('help.html')
 
 @api_bp.route('/api/v1/users')
 def api_v1_users():
@@ -16,7 +16,7 @@ def api_v1_users():
     users = db.sql_fetch("SELECT * from users;")
     header = ["id", "name", "email", "password", "nif", "admin"]
 
-    # dicio = []
+    dicio = []
     for user in users:
         dicio.append( dict( zip(header, user)) )
 
